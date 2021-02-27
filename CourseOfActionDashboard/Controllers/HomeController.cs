70eb71d95667f20845530a00e9be9412269a03e8
DavidDSS <daviddss1999@hotmail.com>
2021-02-27 12:23:41 -0500

    Added Login Page and Student Class
    -Missing Database Access

--- CourseOfActionDashboard/Controllers/HomeController.cs
+++ CourseOfActionDashboard/Controllers/HomeController.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CourseOfActionDashboard.Models;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
 
 namespace CourseOfActionDashboard.Controllers
-{
+{ 
     public class HomeController : Controller
     {
+        private DB_Entities _db = new DB_Entities();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -26,6 +31,60 @@ namespace CourseOfActionDashboard.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LoginPage(string email, string password)
+        {
+            if (ModelState.IsValid && password!=null)
+            {
+                var f_password = GetMD5(password);
+                var data = _db.Students.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    /*Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idStudent"] = data.FirstOrDefault().idStudent;*/
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("LoginPage");
+                }
+            }
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {            
+            HttpContext.Session.Clear();//remove session
+            return RedirectToAction("LoginPage");
+        }
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- CourseOfActionDashboard/Startup.cs
+++ CourseOfActionDashboard/Startup.cs
@@ -24,12 +24,16 @@ namespace CourseOfActionDashboard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddSession();
+            services.AddMvc();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSession();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
