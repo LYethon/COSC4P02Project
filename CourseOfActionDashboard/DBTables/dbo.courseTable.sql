f19fd14e312c3057fd099f7870277c5ad7a6f721
Liam Yethon <49956919+LYethon@users.noreply.github.com>
2021-03-26 13:01:40 -0400

    Course Table database files

---
+++ CourseOfActionDashboard/DBTables/dbo.courseTable.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[courseTable] (
+    [CID]                 INT         NOT NULL,
+    [Name]                CHAR (100)  NOT NULL,
+    [Code]                CHAR (15)   NOT NULL,
+    [Subject]             CHAR (10)   NOT NULL,
+    [AlternativeSubject1] CHAR (10)   NULL,
+    [AlternativeSubject2] CHAR (10)   NULL,
+    [Description]         CHAR (2000) NOT NULL,
+    [CreditValue]         FLOAT (53)  NOT NULL,
+    [Prerequisites]       CHAR (100)  NULL,
+    [Context]             CHAR (100)  NULL,
+    PRIMARY KEY CLUSTERED ([CID] ASC)
+);
+
