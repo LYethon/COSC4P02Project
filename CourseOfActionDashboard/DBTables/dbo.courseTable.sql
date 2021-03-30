61cce80814ec6125e270b8b9bc16c0b7d602d774
Liam Yethon <49956919+LYethon@users.noreply.github.com>
2021-03-30 18:13:46 -0400

    CHAR to VARCHAR
    no more whitespaces

--- CourseOfActionDashboard/DBTables/dbo.courseTable.sql
+++ CourseOfActionDashboard/DBTables/dbo.courseTable.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[courseTable] (
     [CID]                 INT         NOT NULL,
-    [Name]                CHAR (100)  NOT NULL,
-    [Code]                CHAR (15)   NOT NULL,
-    [Subject]             CHAR (10)   NOT NULL,
-    [AlternativeSubject1] CHAR (10)   NULL,
-    [AlternativeSubject2] CHAR (10)   NULL,
-    [Description]         CHAR (2000) NOT NULL,
+    [Name]                VARCHAR (100)  NOT NULL,
+    [Code]                VARCHAR (15)   NOT NULL,
+    [Subject]             VARCHAR (10)   NOT NULL,
+    [AlternativeSubject1] VARCHAR (10)   NULL,
+    [AlternativeSubject2] VARCHAR (10)   NULL,
+    [Description]         VARCHAR (2000) NOT NULL,
     [CreditValue]         FLOAT (53)  NOT NULL,
-    [Prerequisites]       CHAR (100)  NULL,
-    [Context]             CHAR (100)  NULL,
+    [Prerequisites]       VARCHAR (100)  NULL,
+    [Context]             VARCHAR (100)  NULL,
     PRIMARY KEY CLUSTERED ([CID] ASC)
 );
 
