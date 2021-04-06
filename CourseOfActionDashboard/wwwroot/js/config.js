82914d6412bdee5c18c2a37da0e8b218425812d6
Katie Lee <g.hyunlee12@gmail.com>
2021-04-06 17:29:39 -0400

    Created config file

---
+++ CourseOfActionDashboard/wwwroot/js/config.js
@@ -0,0 +1,7 @@
+﻿//the main configuration file
+var config = {
+    'max_years': 10,
+    'max_cr_year': 6,
+    'max_cap_cr_year': 7.5,
+    'default_sched': [[1,2,3,44,45],[4,5,6,7,43,46],[11,12,13,41,42],[24,25,31,]]
+}
