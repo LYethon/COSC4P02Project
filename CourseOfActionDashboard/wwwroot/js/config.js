66b0c2cf0ee1693f7f2e6c6254da40d9c5f7cc6c
Katie Lee <g.hyunlee12@gmail.com>
2021-04-13 22:14:22 -0400

    Updated config.js

--- CourseOfActionDashboard/wwwroot/js/config.js
+++ CourseOfActionDashboard/wwwroot/js/config.js
@@ -1,10 +1,9 @@
 ﻿//the main configuration file
 var config = {
     'max_years': 10,
-    'max_cr_year': 4,
-    'max_cap_cr_year': 5,
+    'max_cr_year': 6,
+    'max_cap_cr_year': 12,
     'default_sched': [[1, 2, 3, 44, 45], [4, 5, 6, 7, 43, 46], [11, 12, 13, 41, 42], [24, 25, 31,]],
     'social_rec': [107, 108],
     'human_rec': [48, 49]
-
 }
